fae63a3bd658c8f05c7345cd648a05a0b2a5e4cc
senkowo <senkowo@mailbox.org>
2023-05-14 12:32:45 -0400

    termux straight.el and some exwm progress

--- .config/emacs.termux/exwm/install-repeatable.sh
+++ .config/emacs.termux/exwm/install-repeatable.sh
@@ -2,7 +2,7 @@
 pkg upgrade
 
 # install common stuff, plus desktop utils
-pkg install vim emacs git neofetch man openssh cmake libtool \
-    x11-repo termux-api tigervnc
+pkg install vim emacs git neofetch man p7zip openssh cmake libtool \
+    x11-repo termux-api tigervnc xorg-server
 
 pip install yt-dlp
--- .config/emacs.termux/init.el
+++ .config/emacs.termux/init.el
@@ -25,6 +25,26 @@
   (package-install 'use-package))
 (require 'use-package)
 
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	"https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Use straight.el for use-package expressions
+(straight-use-package 'use-package)
+
+;; Load the helper package for commands like `straight-x-clean-unused-repos'
+(require 'straight-x)
+
 ;;
 ;; system --------
 ;;
@@ -395,3 +415,8 @@
 (use-package vterm
   :ensure t
   :commands vterm)
+
+(use-package exwm
+  :ensure t
+  :config
+  (exwm-enable))
