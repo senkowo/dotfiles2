03e4be7eb3a8cb000459f4d15ef789f155203861
senkowo <senkowo@mailbox.org>
2023-05-17 00:34:58 -0400

    smol update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -428,9 +428,10 @@
 (ri/leader-keys
   "sF" 'free-keys)
 
-;;keep cursor at same position when scrolling
+;; keep cursor at same position when scrolling
 (setq scroll-preserve-screen-position 1)
-;;scroll window up/down by one line
+;; scroll window up/down by one line
+;; FIX this by changing C-M-g to command name "universal-argument"?
 (global-set-key (kbd "M-n") (kbd "C-M-g 1 C-v"))
 (global-set-key (kbd "M-p") (kbd "C-M-g 1 M-v"))
 
